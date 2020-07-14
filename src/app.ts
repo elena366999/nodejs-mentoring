643ff657ff85a36760292b47b0a468933196fb05
Alena Semianitskaya <Alena_Semianitskaya@epam.com>
2020-07-14 11:11:14 +0300

    Remove redundant middleware

--- src/app.ts
+++ src/app.ts
@@ -29,13 +29,6 @@ class App {
     }
 
     private commonConfig(): void {
-        this.app.use((req, res, next) => {
-            res.header('Access-Control-Allow-Origin', '*');
-            res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-            next();
-        });
-
         this.app.use(cors());
         this.app.use(express.json());
         this.app.use(express.urlencoded({ extended: false }));
